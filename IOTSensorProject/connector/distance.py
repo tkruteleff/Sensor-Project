78a76895e4db54c68d144f3b858e13a19cd10b4a
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-05 13:20:28 +0300

    Changes to distance.py

--- IOTSensorProject/connector/distance.py
+++ IOTSensorProject/connector/distance.py
@@ -7,8 +7,11 @@ from DBConnector import insert
 
 GPIO.setmode(GPIO.BCM)
 
+#Defineing Trigger and Echo pin locations on Rasp
 pinTrigger = 19
 pinEcho = 13
+pinRTrigger = 23
+pinREcho = 24
 
 def close(signal, frame):
 	print("\nTurning off ultrasonic distance detection...\n")
@@ -17,25 +20,29 @@ def close(signal, frame):
 
 signal.signal(signal.SIGINT, close)
 
+#Setting up the trigger and echo pin Outputs and Inputs
 GPIO.setup(pinTrigger, GPIO.OUT)
 GPIO.setup(pinEcho, GPIO.IN)
+GPIO.setup(pinRTrigger, GPIO.OUT)
+GPIO.setup(pinREcho, GPIO.IN)
 
 while True:
 
 	GPIO.output(pinTrigger, True)
-	
+	GPIO.output(pinRTrigger, True)
 	time.sleep(0.00001)
 	GPIO.output(pinTrigger, False)
+    GPIO.output(pinRTrigger, False)
 
 	startTime = time.time()
 	stopTime = time.time()
 
 	# save start time
-	while 0 == GPIO.input(pinEcho):
+	while 0 == GPIO.input(pinEcho) & 0 == GPIO.input(pinREcho):
 		startTime = time.time()
 
 	# save time of arrival
-	while 1 == GPIO.input(pinEcho):
+	while 1 == GPIO.input(pinEcho) & 1 == GPIO.input(pinREcho):
 		stopTime = time.time()
 
 	# time difference between start and arrival
