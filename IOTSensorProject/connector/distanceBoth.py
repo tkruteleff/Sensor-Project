614233ed5a55add79d420c911462955f03b6a80e
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-12 15:26:11 +0300

    Removed Threading from distanceBoth.py

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -1,10 +1,6 @@
 import distance
 import distanceRight
-from threading import Thread
-
-t1 = Thread(target = distance)
-t2 = Thread(target = distanceRight)
 
 while True:
-    t1.start()
-    t2.start()
+    distance.SensorRangeLeft()
+    distanceRight.SensorRangeRight()
