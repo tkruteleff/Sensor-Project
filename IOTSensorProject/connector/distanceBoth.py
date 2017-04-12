5c71140f785a3c1e32ce05f73d3d826a040c8e1a
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-12 15:15:27 +0300

    Moved the while-function in distanceBoth.py

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -2,9 +2,9 @@ import distance
 import distanceRight
 from threading import Thread
 
-while True:
-    t1 = Thread(target = distance)
-    t2 = Thread(target = distanceRight)
+t1 = Thread(target = distance)
+t2 = Thread(target = distanceRight)
 
+while True:
     t1.start()
     t2.start()
