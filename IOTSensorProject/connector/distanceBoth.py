8e846b6f7c93e0803f48d38ad54953032a892455
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-19 13:20:36 +0300

    Testing

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -1,6 +1,13 @@
 from distance import *
 from distanceRight import *
 
+left = SensorRangeLeft().distanceLeftC
+right = SensorRangeRight().distanceRightC
+
+
 while True:
     SensorRangeLeft()
     SensorRangeRight()
+
+    if left <= 50:
+        print("1")
