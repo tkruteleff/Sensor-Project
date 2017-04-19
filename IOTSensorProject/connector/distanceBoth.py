dbe591a65fe02a2bba6f18e030ac23f353e58533
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-19 12:34:29 +0300

    Added if-functions to the distanceBoth -class

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -4,3 +4,8 @@ import distanceRight
 while True:
     distance.SensorRangeLeft()
     distanceRight.SensorRangeRight()
+
+    #Vasemman laskeminen
+    if(distance.SensorRangeLeft().distanceLeft() < 50):
+        if(distanceRight.SensorRangeRight().distanceRight < 50):
+            print ("1")
