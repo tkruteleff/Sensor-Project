e7eac731c411f6a1179a192441972c420d5e758d
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-24 12:41:31 +0300

    Added print clauses to dinstanceBoth

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -6,5 +6,9 @@ while True:
     SensorRangeLeft()
     SensorRangeRight(distanceRightC)
 
+    print("This is the distance of " + distanceRightC)
+
     if distanceRightC > 50:
         print ("1")
+
+    
