e407915d2074d1354d5127fe1297bfc311404f72
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-24 13:46:57 +0300

    Small changes to all files

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -7,14 +7,13 @@ while True:
     SensorRangeLeft()
     SensorRangeRight()
 
-    if SensorRangeRight() < 60:
-        if SensorRangeLeft() < 60:
+    if SensorRangeRight() > 60:
+        if SensorRangeLeft() > 60:
             print ("1")
             insert1()
 
 
-
-    if SensorRangeLeft() < 60:
-        if SensorRangeRight() < 60:
+    if SensorRangeLeft() > 60:
+        if SensorRangeRight() > 60:
             print("2")
             insert2()
