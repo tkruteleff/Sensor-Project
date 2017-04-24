415ef6dd71ea96b905f71f46eac6643655aac34c
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-24 13:11:24 +0300

    Added a second if clause

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -6,8 +6,10 @@ while True:
     SensorRangeLeft()
     SensorRangeRight()
 
-    print("This is the distance of " + str(SensorRangeRight()))
-    print("This is the distance of " + str(SensorRangeLeft()))
     if SensorRangeRight() < 60:
         if SensorRangeLeft() < 60:
             print ("1")
+
+    if SensorRangeLeft() < 60:
+        if SensorRangeRight() < 60:
+            print("2")
