3e81ac776610974d8d673ab78eeba725b830ae40
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-24 15:14:48 +0300

    Small changes to the distanceBoth

--- IOTSensorProject/connector/distance.py
+++ IOTSensorProject/connector/distance.py
@@ -5,44 +5,46 @@ import sys
 
 GPIO.setmode(GPIO.BCM)
 
-#Defineing Trigger and Echo pin locations on Rasp
+# Defineing Trigger and Echo pin locations on Rasp
 pinTriggerLeft = 19
 pinEchoLeft = 13
 
+
 def close(signal, frame):
-	print("\nTurning off ultrasonic distance detection...\n")
-	GPIO.cleanup()
-	sys.exit(0)
+    print("\nTurning off ultrasonic distance detection...\n")
+    GPIO.cleanup()
+    sys.exit(0)
+
 
 signal.signal(signal.SIGINT, close)
 
-#Setting up the trigger and echo pin Outputs and Inputs
+# Setting up the trigger and echo pin Outputs and Inputs
 GPIO.setup(pinTriggerLeft, GPIO.OUT)
 GPIO.setup(pinEchoLeft, GPIO.IN)
-
 
 GPIO.output(pinTriggerLeft, True)
 time.sleep(0.00001)
 GPIO.output(pinTriggerLeft, False)
 
+
 def SensorRangeLeft():
-	GPIO.output(pinTriggerLeft,True)
-	time.sleep(0.00001)
-	GPIO.output(pinTriggerLeft, False)
-	startTimeLeft = time.time()
-	stopTimeLeft = time.time()
+    GPIO.output(pinTriggerLeft, True)
+    time.sleep(0.00001)
+    GPIO.output(pinTriggerLeft, False)
+    startTimeLeft = time.time()
+    stopTimeLeft = time.time()
 
-	while 0 == GPIO.input(pinEchoLeft):
-			startTimeLeft = time.time()
+    while 0 == GPIO.input(pinEchoLeft):
+        startTimeLeft = time.time()
 
-	while 1 == GPIO.input(pinEchoLeft):
-		stopTimeLeft = time.time()
+    while 1 == GPIO.input(pinEchoLeft):
+        stopTimeLeft = time.time()
 
-	TimeElapsedLeft = stopTimeLeft - startTimeLeft
+    TimeElapsedLeft = stopTimeLeft - startTimeLeft
 
-	distanceLeftC = (TimeElapsedLeft * 34300) /2
+    distanceLeftC = (TimeElapsedLeft * 34300) / 2
 
-	print ("DistanceLeft: %.1f cm" % distanceLeftC)
-	time.sleep(1)
+    print ("DistanceLeft: %.1f cm" % distanceLeftC)
+    time.sleep(1)
 
-	return distanceLeftC
+    return distanceLeftC
--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -2,16 +2,14 @@ from distance import *
 from distanceRight import *
 from DBConnector import *
 
-
 while True:
-    SensorRangeLeft()
-    SensorRangeRight()
+    #SensorRangeLeft()
+    #SensorRangeRight()
 
     if SensorRangeRight() > 60:
         if SensorRangeLeft() > 60:
             print ("1")
             insert1()
-
 
     if SensorRangeLeft() > 60:
         if SensorRangeRight() > 60:
