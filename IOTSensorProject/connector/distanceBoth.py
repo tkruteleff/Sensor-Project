45c02808c35fcb10f473d7b6163d06f22354e874
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-24 15:19:40 +0300

    Changed the second if clause to elif

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -10,8 +10,7 @@ while True:
         if SensorRangeLeft() > 60:
             print ("1")
             insert1()
-
-    if SensorRangeLeft() > 60:
+    elif SensorRangeLeft() > 60:
         if SensorRangeRight() > 60:
             print("2")
             insert2()
