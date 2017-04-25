1e8f77d03555e50d4cdec5ecf737f76b989629bd
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-25 14:27:23 +0300

    Added visitorCount variable

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -1,19 +1,28 @@
 from distance import *
 from distanceRight import *
 from DBConnector import *
-import time
 
-localtime = time.asctime(time.localtime(time.time()))
 maxDistance = 90
-
-print(localtime)
+visitorCount = 0
 
 while True:
     if SensorRangeRight() < maxDistance:
         if SensorRangeLeft() < maxDistance:
             print ("1")
+            visitorCount = visitorCount + 1
             # insert1()
+            print (visitorCount)
     elif SensorRangeLeft() < maxDistance:
         if SensorRangeRight() < maxDistance:
             print("2")
+            visitorCount = visitorCount - 1
             # insert2()
+            print (visitorCount)
+    if SensorRangeRight() & SensorRangeLeft() < maxDistance:
+        print("1")
+        visitorCount = visitorCount + 1
+        print (visitorCount)
+    elif SensorRangeLeft() & SensorRangeRight() < maxDistance:
+        print ("2")
+        visitorCount = visitorCount -1
+        print (visitorCount)
