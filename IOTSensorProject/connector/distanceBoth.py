f65cb70c26a26d887debdb0fbfd95d97c6342b9b
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-25 13:04:17 +0300

    Changed the if clause

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -3,11 +3,11 @@ from distanceRight import *
 from DBConnector import *
 
 while True:
-    if SensorRangeRight() > 60:
-        if SensorRangeLeft() > 60:
+    if SensorRangeRight() < 60:
+        if SensorRangeLeft() < 60:
             print ("1")
             #insert1()
-    elif SensorRangeLeft() > 60:
-        if SensorRangeRight() > 60:
+    elif SensorRangeLeft() < 60:
+        if SensorRangeRight() < 60:
             print("2")
             #insert2()
