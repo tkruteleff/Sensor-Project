e3ee8efd3008cdbaf861500704960f96be1ea500
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-25 13:02:52 +0300

    Small changes to the distanceBoth

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -3,14 +3,11 @@ from distanceRight import *
 from DBConnector import *
 
 while True:
-    #SensorRangeLeft()
-    #SensorRangeRight()
-
     if SensorRangeRight() > 60:
         if SensorRangeLeft() > 60:
             print ("1")
-            insert1()
+            #insert1()
     elif SensorRangeLeft() > 60:
         if SensorRangeRight() > 60:
             print("2")
-            insert2()
+            #insert2()
