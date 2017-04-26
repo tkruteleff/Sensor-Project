c1fe584eff09f9dd6d9766813033a98e7bedc41d
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-26 12:57:28 +0300

    Changes

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -7,8 +7,8 @@ import time
 maxDistance = 90
 
 while True:
-    if SensorRangeRight() < maxDistance & time.time() > 5:
-        if SensorRangeLeft() < maxDistance & time.time() > 5:
+    if SensorRangeRight() < maxDistance & time.time() != 5:
+        if SensorRangeLeft() < maxDistance & time.time() != 5:
             Variables.visitorCount = Variables.visitorCount + 1
             print (Variables.visitorCount)
             DBConnector.insertIncrease()
