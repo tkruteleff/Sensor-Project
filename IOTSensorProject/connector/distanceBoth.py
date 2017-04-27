bb475fdd2eedd29b344450a5b01da5224d812540
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-27 14:59:43 +0300

    Returned to previous

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -7,13 +7,13 @@ while True:
 
     SensorRangeLeft(), SensorRangeRight()
 
-    while Variables.distanceRightC < Variables.maxDistance:
+    if Variables.distanceRightC < Variables.maxDistance:
             if Variables.distanceLeftC < Variables.maxDistance:
                 Variables.visitorCount = Variables.visitorCount + 1
                 print (Variables.visitorCount)
                 DBConnector.insertIncrease()
 
-    while Variables.distanceLeftC < Variables.maxDistance:
+    elif Variables.distanceLeftC < Variables.maxDistance:
         if Variables.distanceRightC < Variables.maxDistance:
             if Variables.visitorCount > 0:
                 Variables.visitorCount = Variables.visitorCount - 1
