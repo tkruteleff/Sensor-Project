53520a26474489ee74f41881b3df5895164af4b9
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-04-27 14:55:03 +0300

    Removed inner whiles

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -8,13 +8,13 @@ while True:
     SensorRangeLeft(), SensorRangeRight()
 
     while Variables.distanceRightC < Variables.maxDistance:
-            while Variables.distanceLeftC < Variables.maxDistance:
+            if Variables.distanceLeftC < Variables.maxDistance:
                 Variables.visitorCount = Variables.visitorCount + 1
                 print (Variables.visitorCount)
                 DBConnector.insertIncrease()
 
     while Variables.distanceLeftC < Variables.maxDistance:
-        while Variables.distanceRightC < Variables.maxDistance:
+        if Variables.distanceRightC < Variables.maxDistance:
             if Variables.visitorCount > 0:
                 Variables.visitorCount = Variables.visitorCount - 1
                 print (Variables.visitorCount)
