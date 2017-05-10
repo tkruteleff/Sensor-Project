2c7f796fe7f19a9240bb4336263bacf988ad7bf3
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-05-10 12:20:04 +0300

    Changes

--- IOTSensorProject/connector/Variables.py
+++ IOTSensorProject/connector/Variables.py
@@ -2,9 +2,3 @@ visitorCount = 0
 maxDistance = 50
 distanceLeftC = 0
 distanceRightC = 0
-
-setattr(object, 'distanceLeftC', 0)
-getattr(object, 'distanceLeftC')
-
-setattr(object, 'distanceRightC', 0)
-getattr(object, 'distanceRightC')
