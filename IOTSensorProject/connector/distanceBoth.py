eb7d28d51f1e62859caa5795d870b578a12b8484
GarStiver <teemu.kruteleff@myy.haaga-helia.fi>
2017-05-10 15:05:24 +0300

    Small changes

--- IOTSensorProject/connector/distanceBoth.py
+++ IOTSensorProject/connector/distanceBoth.py
@@ -3,11 +3,14 @@ from distance import *
 import Variables
 import DBConnector
 
+
 def checkLeft():
     if SensorRangeRight(dright) < Variables.maxDistance:
-        Variables.visitorCount = Variables.visitorCount - 1
-        print (Variables.visitorCount)
-        DBConnector.insertDelete()
+        if Variables.visitorCount >= 0:
+            Variables.visitorCount = Variables.visitorCount - 1
+            print (Variables.visitorCount)
+            DBConnector.insertDelete()
+
 
 def checkRight():
     if SensorRangeLeft(dleft) < Variables.maxDistance:
@@ -15,17 +18,18 @@ def checkRight():
         print(Variables.visitorCount)
         DBConnector.insertIncrease()
 
+
 while True:
 
     if SensorRangeLeft(dleft) < Variables.maxDistance:
         checkLeft()
         # if SensorRangeRight(dright) < Variables.maxDistance:
-             #   Variables.visitorCount = Variables.visitorCount + 1
-              ## DBConnector.insertIncrease()
+        #   Variables.visitorCount = Variables.visitorCount + 1
+        ## DBConnector.insertIncrease()
 
     if SensorRangeRight(dright) < Variables.maxDistance:
         checkRight()
-        #if SensorRangeLeft(dleft) < Variables.maxDistance:
-         #   if Variables.visitorCount > 0:
-          #      Variables.visitorCount = Variables.visitorCount - 1
-           ##    DBConnector.insertDelete()
+        # if SensorRangeLeft(dleft) < Variables.maxDistance:
+        #   if Variables.visitorCount > 0:
+        #      Variables.visitorCount = Variables.visitorCount - 1
+        ##    DBConnector.insertDelete()
